63908968a93e27d373d120c6bb5b8f9165910824
kwon woo <dirn0568@naver.com>
2024-03-12 00:48:26 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import './index.js';
+
+function App() {
+  return (
+    React.createElement("h1", null, "Hello World")
+  );
+}
+
+export default App;
